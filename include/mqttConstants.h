f51e4ad3ece32793849af2e87aa385594fa7fcd4
Lenard Valdez <iamlele17@gmail.com>
2021-07-15 13:13:28 +0800

    change local server

--- include/mqttConstants.h
+++ include/mqttConstants.h
@@ -7,13 +7,13 @@
 
 //Ethernet Variable for connecting
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-IPAddress ip(192, 168, 1, 117);
+IPAddress ip(192, 168, 1, 118);
 IPAddress myDns(192, 168, 1, 4);
 IPAddress gateway(192, 168, 1, 1);
 IPAddress subnet(255, 255, 255, 0);
 
 // MQTT broker host/IP and topic subscription
-const char* server = "192.168.1.65";
+const char* server = "192.168.1.4";
 // const char* server = "mqtts.hydr-a.local";
 const char* topic_sensor_data = "hyd-1/sensor_data/";
 const char* topic_probe_data = "hyd-1/probe_data/";
